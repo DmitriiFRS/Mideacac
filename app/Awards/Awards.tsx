20bb083a2fe087ec120a5f10f4097d4df2b30fc4
Дмитрий <Dmitriifrs@gmail.com>
2023-08-28 17:40:54 +0500

    added awards block

--- app/Awards/Awards.tsx
+++ app/Awards/Awards.tsx
@@ -1,9 +1,22 @@
 import styles from './Awards.module.scss';
+import Image from 'next/image';
+import ahri from '../../public/icons/AwardsIcons/AHRI.png';
+import iso from '../../public/icons/AwardsIcons/ISO.png';
+import eurovent from '../../public/icons/AwardsIcons/EUROVENT.png';
+import gold2022 from '../../public/icons/AwardsIcons/GOLD 2022.png';
+
 
 function Awards() {
 return (
    <section className={styles.awards}>
-      Awards
+      <h2 className={styles.awards__title}>Награды и сертификаты</h2>
+      <div className={styles.awards__iconsBody}>
+         <Image src={ahri} alt='' />
+         <Image src={iso} alt='' />
+         <Image src={eurovent} alt='' />
+         <Image src={gold2022} alt='' />
+         <Image src={eurovent} alt='' />
+      </div>
    </section>
 )
 }
--- app/Machines/NewestMachines.tsx
+++ app/Machines/NewestMachines.tsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import styles from './Machines.module.scss';
 import Image from 'next/image';
 import { RootState } from '../Redux/store';
+import Link from 'next/link';
 
 type NewestMachinesPropsType = {
    deviceName: string
@@ -14,12 +15,12 @@ type NewestMachinesPropsType = {
 function NewestMachines({deviceName, image, imageWidth, imageHeight}: NewestMachinesPropsType) {
    const visibility = useSelector((state: RootState) => state.mainPageReducer.isThirdAction);
    return (
-      <div className={`${imageHeight === 302 ? styles.machines__newest1 : styles.machines__newest2} ${visibility ? styles.active : ''}`}>
-         <p className={styles.machines__newest__title}>{deviceName}</p>
+      <Link href='/VRF' className={`${imageHeight === 302 ? styles.machines__newest1 : styles.machines__newest2} ${visibility ? styles.active : ''}`}>
+         <p className={`${styles.machines__newest__title} ${imageHeight === 302 ? styles.machines__newest__title1 : styles.machines__newest__title2} ${visibility ? styles.titleActive : ''}`}>{deviceName}</p>
          <div className={styles.machines__container}>
             <Image src={image} alt='magboost' width={imageWidth} height={imageHeight} />
          </div>
-      </div>
+      </Link>
    )
 }
 export default NewestMachines;
