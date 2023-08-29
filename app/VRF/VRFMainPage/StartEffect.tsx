15032383d920bad23ffbd119389b88114c4ea79f
Дмитрий <Dmitriifrs@gmail.com>
2023-08-29 15:02:33 +0500

    working on effect

---
+++ app/VRF/VRFMainPage/StartEffect.tsx
@@ -0,0 +1,11 @@
+import styles from '../VRFv8Page.module.scss';
+
+function StartEffect() {
+return (
+   <div className={styles.effect}>
+      <h2 className={styles.effect__title}>Magboost</h2>
+      <h3 className={styles.effect__bgTitle}>A legendary</h3>
+   </div>
+)
+}
+export default StartEffect;
--- app/VRF/VRFMainVideo.tsx
+++ app/VRF/VRFMainVideo.tsx
@@ -1,10 +1,15 @@
+import StartEffect from './VRFMainPage/StartEffect';
 import styles from './VRFv8Page.module.scss';
 
 function VRFMainVideo() {
    return (
-      <video className={styles.vrf__videoContainer} autoPlay muted loop>
-         <source className={styles.video} src='/video/vrf-video.mp4' type='video/mp4' />
-      </video>
+      <div className={styles.vrf__container}>
+         <StartEffect />
+         <video className={styles.vrf__videoContainer} autoPlay muted loop>
+            <source className={styles.video} src='/video/vrf-video.mp4' type='video/mp4' />
+         </video>
+      </div>
+      
    )
 }
 export default VRFMainVideo;
