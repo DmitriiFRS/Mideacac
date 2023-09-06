2fd3f508f972354c121e61a33c9bad4c1327c689
Дмитрий <Dmitriifrs@gmail.com>
2023-09-06 15:55:02 +0500

    bugfix

--- app/Footer/Footer.tsx
+++ app/Footer/Footer.tsx
@@ -12,7 +12,28 @@ function Footer() {
                </div>
                <h3 className={styles.footer__logo__title}>Авторские права Midea, 2023 г. Все права защищены. Политика конфиденциальности</h3>
             </div>
-            <div className={styles.footer__contacts}></div>
+            <div className={styles.footer__address}>
+               <ul className={styles.footer__address__list}>
+                  <li className={styles.footer__address__item}>Улица Карасарай дом 2 Олмазарский р-н</li>
+                  <li className={styles.footer__address__item}>Улица Лабихавуз 57 Чиланзарский р-н 1 квартал</li>
+                  <li className={styles.footer__address__item}>г. Ташкент, Яккасарайский р-н, ул. Шота Руставели, 81</li>
+               </ul>
+            </div>
+            <div className={styles.footer__phones}>
+               <ul className={styles.footer__phones__list}>
+                  <li className={styles.footer__phones__item}>+998 (93) 111-11-11</li>
+                  <li className={styles.footer__phones__item}>+998 (93) 111-11-11</li>
+                  <li className={styles.footer__phones__item}>+998 (93) 111-11-11</li>
+               </ul>
+            </div>
+            <div className={styles.footer__other}>
+               <ul className={styles.footer__other__list}>
+                  <li className={styles.footer__other__item}>ООО «WELKIN climate solutions» 100 115, Labihavuz street, 57, Tashkent</li>
+                  <li className={styles.footer__other__item}>ИНН: 306 478 365 ОКЭД 46490</li>
+                  <li className={styles.footer__other__item}>ТГФ АКБ «Капиталбанк» МФО: 00445</li>
+                  <li className={styles.footer__other__item}>р/с: 2020 8000 9050 8878 0001</li>
+               </ul>
+            </div>
          </div>
          
       </footer>
--- app/Innovation/Arrow.tsx
+++ app/Innovation/Arrow.tsx
@@ -1,13 +1,9 @@
 "use client"
-
-import { useSelector } from 'react-redux';
 import styles from './Innovation.module.scss';
-import { RootState } from '../Redux/store';
 
 function Arrow() {
-   const isLastAction = useSelector((state: RootState) => state.mainPageReducer.isThirdAction)
 return (
-   <div className={`${styles.arrows__body} ${isLastAction ? styles.arrows__unactive : ''}`}>
+   <div className={`${styles.arrows__body}`}>
       <div className={styles.arrows__sliding}>
          <div className={styles.arrows__arrow}></div>
       </div>
