c3643abe59d8740f5e3fb3b18f3a400d6a8e4603
Дмитрий <Dmitriifrs@gmail.com>
2023-09-08 09:44:15 +0500

    testing server/client components

--- app/MagboostV8RC/PreviewTitle.tsx
+++ app/MagboostV8RC/PreviewTitle.tsx
@@ -1,25 +1,15 @@
-
+"use client"
 import { useSelector } from 'react-redux';
 import styles from './MagboostV8.module.scss';
 import { RootState } from '@/app/Redux/store';
+import PreviewTitleText from './PreviewTitleText';
 
 
 function PreviewTitle({previewTitle}: {previewTitle: Array<string>}) {
-   let scrollPositionCount = 18.5;
    const scrollPosition = useSelector((state: RootState) => state.innovationsReducer.scrollRate);
    return (
    <div className={styles.preview}>
-      <div className={`${styles.preview__titleContainer}`}>
-         {previewTitle.map((el, index) => {
-            scrollPositionCount += 1;
-            return (
-               <div key={index} className={styles.preview__textLine}>
-                  {el}
-                  <div style={{width: scrollPosition > scrollPositionCount ? '100%' :'0%'}} className={styles.preview__textFiller}>{el}</div>
-               </div>
-            )
-         })}
-      </div>
+      <PreviewTitleText previewTitle={previewTitle} scrollPosition={scrollPosition}/>
    </div>
    )
 }
---
+++ app/MagboostV8RC/PreviewTitleText.tsx
@@ -0,0 +1,19 @@
+import styles from './MagboostV8.module.scss';
+
+function PreviewTitleText({previewTitle, scrollPosition}: {previewTitle: Array<string>, scrollPosition: number}) {
+   let scrollPositionCount = 18.5;
+   return (
+      <div className={`${styles.preview__titleContainer}`}>
+         {previewTitle.map((el, index) => {
+            scrollPositionCount += 1;
+            return (
+               <div key={index} className={styles.preview__textLine}>
+                  {el}
+                  <div style={{width: scrollPosition > scrollPositionCount ? '100%' :'0%'}} className={styles.preview__textFiller}>{el}</div>
+               </div>
+            )
+         })}
+      </div>
+   )
+}
+export default PreviewTitleText;
