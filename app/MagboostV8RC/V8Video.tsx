9330c3b1d897dec1f1582c099bf326c68256288d
Дмитрий <Dmitriifrs@gmail.com>
2023-10-03 10:11:54 +0500

    dev-rework2

--- app/MagboostV8RC/V8Video.tsx
+++ app/MagboostV8RC/V8Video.tsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import StartEffect from './StartEffect';
 import styles from './MagboostV8.module.scss';
 import { RootState } from '@/app/Redux/store';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 type VideoComponentType = {
    video: string
@@ -13,14 +13,15 @@ type VideoComponentType = {
 
 function V8Video({video, modelStartText}: VideoComponentType) {
    const scrollPosition = useSelector((state: RootState) => state.innovationsReducer.scrollRate);
+   const videoRef = useRef<any>(undefined);
    useEffect(() => {
-      console.log(scrollPosition);
-   },[scrollPosition])
+      videoRef.current.defaultMuted = true
+   })
    return (
       <div className={styles.vrf__container}>
          <StartEffect modelStartText={modelStartText} />
          <video style={{position: scrollPosition < 14.5 ? 'fixed' : 'absolute'}}
-         className={`${styles.vrf__videoContainer} ${scrollPosition < 14.5 ? styles.vrf__videoContainer__unactive : ''}`} autoPlay muted loop>
+         className={`${styles.vrf__videoContainer} ${scrollPosition < 14.5 ? styles.vrf__videoContainer__unactive : ''}`} ref={videoRef} autoPlay muted loop playsInline>
             <source className={styles.video} src={video} type='video/mp4' />
          </video>
       </div>
