abdf7ca5c5277a2a5640fccfef521e5b8dfd0b4d
Дмитрий <Dmitriifrs@gmail.com>
2023-10-05 15:01:43 +0500

    fix

--- app/Awards/Awards.tsx
+++ app/Awards/Awards.tsx
@@ -1,21 +1,21 @@
 import styles from './Awards.module.scss';
 import Image from 'next/image';
-import ahri from '../../public/icons/AwardsIcons/AHRI.png';
-import iso from '../../public/icons/AwardsIcons/ISO.png';
-import eurovent from '../../public/icons/AwardsIcons/EUROVENT.png';
-import gold2022 from '../../public/icons/AwardsIcons/GOLD 2022.png';
+import ahri from '../../public/icons/AwardsIcons/ahri.svg';
+import iso from '../../public/icons/AwardsIcons/iso.svg';
+import eurovent from '../../public/icons/AwardsIcons/eurovent.svg';
+import gold2022 from '../../public/icons/AwardsIcons/gold.svg';
+import '../globals.css'
 
 
 function Awards() {
 return (
    <section className={styles.awards}>
       <h2 className={styles.awards__title}>Награды и сертификаты</h2>
-      <div className={styles.awards__iconsBody}>
+      <div className={`${styles.awards__iconsBody} ${'container'}`}>
          <Image src={ahri} alt='logo' />
          <Image src={iso} alt='logo' />
          <Image src={eurovent} alt='logo' />
          <Image src={gold2022} alt='logo' />
-         <Image src={eurovent} alt='logo' />
       </div>
    </section>
 )
