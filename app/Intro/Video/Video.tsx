5cb1d886802724acdadf68b62f64b6b6d79211d5
Дмитрий <Dmitriifrs@gmail.com>
2023-11-20 11:09:34 +0500

    some fixes

--- app/Intro/Video/Video.tsx
+++ app/Intro/Video/Video.tsx
@@ -1,13 +1,25 @@
-"use client"
-import { useSelector } from 'react-redux';
-import styles from '../Intro.module.scss';
-import { RootState } from '@/app/Redux/store';
+"use client";
+import { useSelector } from "react-redux";
+import styles from "../Intro.module.scss";
+import { RootState } from "@/app/Redux/store";
 function Video() {
-   const scrollRate = useSelector((state: RootState) => state.mainPageReducer.scrollRate);
-   return (
-      <video style={{opacity: scrollRate !== null && scrollRate > 1 ? 0 : 1}} className={styles.video__container} autoPlay muted loop>
-         <source className={styles.video} src='/video/Intro.mp4' type='video/mp4' />
-      </video>
-   )
+  const scrollRate = useSelector(
+    (state: RootState) => state.mainPageReducer.scrollRate
+  );
+  return (
+    <video
+      style={{ opacity: scrollRate !== null && scrollRate > 0.3 ? 0 : 1 }}
+      className={styles.video__container}
+      autoPlay
+      muted
+      loop
+    >
+      <source
+        className={styles.video}
+        src="/video/Intro.mp4"
+        type="video/mp4"
+      />
+    </video>
+  );
 }
 export default Video;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,56 +1,58 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { STIX_Two_Text, M_PLUS_1, Didact_Gothic } from 'next/font/google'
-import localFont from 'next/font/local'
-import Header from './Header/Header';
-import SideMenu from './Aside/SideMenu';
-import ReduxProvider from './Redux/Provider';
-import Footer from './Footer/Footer';
+import "./globals.css";
+import type { Metadata } from "next";
+import { STIX_Two_Text, M_PLUS_1, Didact_Gothic } from "next/font/google";
+import localFont from "next/font/local";
+import Header from "./Header/Header";
+import SideMenu from "./Aside/SideMenu";
+import ReduxProvider from "./Redux/Provider";
+import Footer from "./Footer/Footer";
 
 const avantGarde = localFont({
-  src: '../public/fonts/avantgardebkbtrusbyme_demi.otf',
-  variable: '--font-avant_garde'
+  src: "../public/fonts/avantgardebkbtrusbyme_demi.otf",
+  variable: "--font-avant_garde",
 });
 const didactGothic = localFont({
-  src: '../public/fonts/DidactGothic-Regular.ttf',
-  variable: '--font-Didact_Gothic'
-})
+  src: "../public/fonts/DidactGothic-Regular.ttf",
+  variable: "--font-Didact_Gothic",
+});
 const mplusone = M_PLUS_1({
-  subsets: ['latin'],
-  style: 'normal',
-  display: 'swap',
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-M_PLUS_1'
-})
+  subsets: ["latin"],
+  style: "normal",
+  display: "swap",
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-M_PLUS_1",
+});
 
 export const stixTwoText = STIX_Two_Text({
-    subsets: ['latin', 'cyrillic'],
-    style: 'normal',
-    display: 'swap',
-    weight: ['400', '500', '600', '700']
-})
+  subsets: ["latin", "cyrillic"],
+  style: "normal",
+  display: "swap",
+  weight: ["400", "500", "600", "700"],
+});
 export const metadata: Metadata = {
-  title: 'Midea CAC',
-  description: 'Midea',
-}
+  title: "Midea CAC",
+  description: "Midea",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html>
-      <body className={`${didactGothic.variable} ${mplusone.variable} ${avantGarde.variable}`}>
+      <body
+        className={`${didactGothic.variable} ${mplusone.variable} ${avantGarde.variable}`}
+      >
         <div className="wrapper">
           <ReduxProvider>
             <Header />
-            <SideMenu/>
+            <SideMenu />
             <main>{children}</main>
             <Footer />
           </ReduxProvider>
         </div>
       </body>
     </html>
-  )
+  );
 }
