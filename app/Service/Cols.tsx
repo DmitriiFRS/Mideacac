f283b4743b67b8ecec7d11294cf338f426b05e3f
Дмитрий <Dmitriifrs@gmail.com>
2023-11-28 16:41:30 +0500

    added animations to service page

--- app/Service/Cols.tsx
+++ app/Service/Cols.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import styles from "./Service.module.scss";
+import { useInView } from "react-intersection-observer";
 
 type ColsProps = {
    title: string;
@@ -6,13 +10,26 @@ type ColsProps = {
 };
 
 function Cols({ cols }: { cols: Array<ColsProps> }) {
+   const [isInView, setIsView] = useState<boolean>(false);
+   const { ref, inView } = useInView({
+      threshold: 0.2,
+      triggerOnce: true,
+   });
+   useEffect(() => {
+      setIsView(inView);
+   }, [inView]);
    return (
       <div className={styles.cols}>
          <h2 className={styles.cols__title}>Долгосрочная гарантия и надежная поддержка</h2>
-         <ul className={styles.cols__list}>
+         <ul ref={ref} className={styles.cols__list}>
             {cols.map((el, index) => {
                return (
-                  <li key={index} className={styles.cols__item}>
+                  <li
+                     key={index}
+                     className={`${styles.cols__item} ${index > 2 ? styles.cols__item2 : ""} ${
+                        isInView ? styles.cols__item__active : ""
+                     }`}
+                  >
                      <p className={styles.cols__item__title}>{el.title}</p>
                      <p className={styles.cols__item__sub}>{el.sub}</p>
                   </li>
--- app/Service/ContentCard.tsx
+++ app/Service/ContentCard.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import styles from "./Service.module.scss";
 import Image, { StaticImageData } from "next/image";
 import cardImg from "../../public/img/OtherPages/serviceCard1.png";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 type ContentCardProps = {
    img: StaticImageData;
@@ -9,9 +13,18 @@ type ContentCardProps = {
    id: number;
 };
 
-function ContentCard({ img, title, list, id }: ContentCardProps) {
+function ContentCard({ img, list, id }: ContentCardProps) {
+   const [isInView, setIsView] = useState<boolean>(false);
+   const { ref, inView } = useInView({
+      threshold: 0.8,
+      triggerOnce: true,
+   });
+
+   useEffect(() => {
+      setIsView(inView);
+   }, [inView]);
    return (
-      <div className={styles.card__cardBody}>
+      <div ref={ref} className={`${styles.card__cardBody} ${isInView ? styles.card__cardBody__active : ""}`}>
          <div className={`${styles.card__imgBody} ${id % 2 === 0 ? styles.card__imgBody__order : ""}`}>
             <Image src={img.src} alt="image" fill />
          </div>
