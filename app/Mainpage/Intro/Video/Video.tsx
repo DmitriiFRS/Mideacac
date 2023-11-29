87f006e21a28d4766801071cb99c14aaa1e877ee
Дмитрий <Dmitriifrs@gmail.com>
2023-11-29 16:47:27 +0500

    fix

--- app/Mainpage/Intro/Video/Video.tsx
+++ app/Mainpage/Intro/Video/Video.tsx
@@ -1,25 +1,27 @@
 "use client";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "../Intro.module.scss";
 import { RootState } from "@/app/Redux/store";
+import { useEffect } from "react";
+import { scrollPosition } from "@/app/Redux/Slice/mainPageSlice";
 function Video() {
-  const scrollRate = useSelector(
-    (state: RootState) => state.mainPageReducer.scrollRate
-  );
-  return (
-    <video
-      style={{ opacity: scrollRate !== null && scrollRate > 0.3 ? 0 : 1 }}
-      className={styles.video__container}
-      autoPlay
-      muted
-      loop
-    >
-      <source
-        className={styles.video}
-        src="/video/Intro.mp4"
-        type="video/mp4"
-      />
-    </video>
-  );
+   const dispatch = useDispatch();
+   const scrollRate = useSelector((state: RootState) => state.mainPageReducer.scrollRate);
+
+   useEffect(() => {
+      dispatch(scrollPosition(window.pageYOffset));
+   }, [dispatch]);
+
+   return (
+      <video
+         style={{ opacity: scrollRate !== null && scrollRate < 0.3 ? 1 : 0 }}
+         className={styles.video__container}
+         autoPlay
+         muted
+         loop
+      >
+         <source className={styles.video} src="/video/Intro.mp4" type="video/mp4" />
+      </video>
+   );
 }
 export default Video;
--- app/hooks/useSessionStorage.tsx
+++ app/hooks/useSessionStorage.tsx
@@ -1,39 +1,27 @@
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { Dispatch, SetStateAction, useState, useEffect } from "react";
 
-export default function useLocalStorage<T>(key: string, initialValue: T): [T, Dispatch<SetStateAction<T>>] {
-  
-    const [storedValue, setStoredValue] = useState(initialValue);
-    const [firstLoadDone, setFirstLoadDone] = useState(false);
-    useEffect(() => {
-        const fromLocal = () => {
-            if (typeof window === 'undefined') {
-                return initialValue;
-            }
-            try {
-                const item = window.localStorage.getItem(key);
-                return item ? JSON.parse(item) as T : initialValue;
-            } catch (error) {
-                console.error(error);
-                return initialValue;
-            }
-        };
-        
-        setStoredValue(fromLocal);
-        setFirstLoadDone(true);
-    }, [initialValue, key]);
+export default function useSessionStorage<T>(key: string, initialValue: T): [T, Dispatch<SetStateAction<T>>] {
+   const [storedValue, setStoredValue] = useState<T>(() => {
+      if (typeof window === "undefined") {
+         return initialValue;
+      }
+      try {
+         const item = window.sessionStorage.getItem(key);
+         return item ? JSON.parse(item) : initialValue;
+      } catch (error) {
+         console.error(error);
+         return initialValue;
+      }
+   });
+   useEffect(() => {
+      if (typeof window !== "undefined") {
+         try {
+            window.sessionStorage.setItem(key, JSON.stringify(storedValue));
+         } catch (error) {
+            console.error(error);
+         }
+      }
+   }, [key, storedValue]);
 
-    useEffect(() => {
-        if (!firstLoadDone) {
-            return;
-        }
-        try {
-            if (typeof window !== 'undefined') {
-                window.localStorage.setItem(key, JSON.stringify(storedValue));
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    }, [storedValue, firstLoadDone, key]);
-    
-    return [storedValue, setStoredValue];
+   return [storedValue, setStoredValue];
 }
