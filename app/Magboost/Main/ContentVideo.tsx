db7aa5a355809bc85c89b974f00bc11036d5fd2b
Дмитрий <Dmitriifrs@gmail.com>
2023-12-02 14:38:09 +0500

    fix

--- app/Magboost/Main/ContentVideo.tsx
+++ app/Magboost/Main/ContentVideo.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import styles from "../Magboost.module.scss";
+import play from "../../../public/icons/MagboostV8/Play.svg";
+import pause from "../../../public/icons/MagboostV8/Pause.svg";
+import Image from "next/image";
 
 function ContentVideo({ video }: { video: string }) {
    const [isPlaying, setPlaying] = useState<boolean>(false);
@@ -17,11 +20,13 @@ function ContentVideo({ video }: { video: string }) {
       }
    }
    return (
-      <div className={styles.bearing__videoContainer}>
+      <div onClick={handlePlay} className={styles.bearing__videoContainer}>
          <video ref={ref} className={styles.bearing__videoBody} loop muted>
             <source className={styles.bearing__video} src={video} />
          </video>
-         <button onClick={handlePlay} className={styles.bearing__playBtn}></button>
+         <button className={styles.bearing__playBtn}>
+            <Image src={isPlaying ? pause : play} alt="play" fill />
+         </button>
       </div>
    );
 }
